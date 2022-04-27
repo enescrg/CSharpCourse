714b096247d1235b767af9040f38036647afbedc
Enes Çirağ <enes65gs@gmail.com>
2022-04-27 09:58:58 +0300

    27.04.22-10:00

---
+++ InfoSourceControlItems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoSourceControlItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  How to: Identify Source Control Item Status in Solution Explorer
+            //  Nasıl yapılır: Solution Explorer'da Kaynak Denetim Öğesi Durumunu Tanımlama
+            //  https://docs.microsoft.com/en-us/previous-versions/ms181372(v=vs.80)?redirectedfrom=MSDN
+        }
+    }
+}
